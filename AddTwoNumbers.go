1bbfd9705b1c2cef27e2fcbfcd24e7501a0e27df
Dmitry Zadoya <52194295+Zadoya@users.noreply.github.com>
2024-04-12 12:23:47 +0300

    Create AddTwoNumbers.go

---
+++ AddTwoNumbers.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+	result := &ListNode{}
+	current := result
+	dec := 0
+	
+	for l1 != nil || l2 != nil || dec != 0 {
+		if l1 != nil {
+            dec += l1.Val
+            l1 = l1.Next
+        }
+        if l2 != nil {
+			dec += l2.Val
+			l2 = l2.Next
+		}
+        current.Val = dec % 10
+		dec = dec / 10
+		if l1 != nil || l2 != nil || dec != 0 {
+			current.Next = &ListNode{}
+			current = current.Next
+		}
+	}
+
+	return result
+}
