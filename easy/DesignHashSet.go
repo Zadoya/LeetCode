0cb062ace78055c50a5a6adf9ca1ca45d113e4b7
Zadoya <zadoya_military@mail.ru>
2024-07-04 10:46:54 +0300

    HashSet

---
+++ easy/DesignHashSet.go
@@ -0,0 +1,25 @@
+// условие задачи - https://leetcode.com/problems/design-hashset/description/
+
+type MyHashSet struct {
+    payload [125001] byte
+}
+
+
+func Constructor() MyHashSet {
+    return MyHashSet{}
+}
+
+
+func (this *MyHashSet) Add(key int)  {
+	this.payload[key / 8] |= 1 << (key % 8) 
+}
+
+
+func (this *MyHashSet) Remove(key int)  {
+    this.payload[key / 8] &= ^(1 << (key % 8))
+}
+
+
+func (this *MyHashSet) Contains(key int) bool {
+	return this.payload[key / 8] & (1 << (key % 8)) != 0
+}
